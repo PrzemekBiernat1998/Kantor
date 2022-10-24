aeb8cf42595a26259d88fb586cb1306564afc587
PrzemekBiernat1998 <przemek.biernat98@gmail.com>
2022-10-24 20:29:24 +0200

    Added og:image, icon, changed variables

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 
-let valueElement = document.querySelector(".js-value");
-let formElement = document.querySelector(".js-form");
-let exchangeResultElement = document.querySelector(".js-exchangeResult");
-let exchangeElement = document.querySelector(".js-exchange");
+{
 
-document.addEventListener("submit", (event) => {
-    event.preventDefault();
-
-
-    let value = valueElement.value;
-    let exchange = exchangeElement.value;
-
-    let exchangeResult = value / exchange;
-
-    exchangeResultElement.innerText = exchangeResult.toFixed(2);
-});
+    const valueElement = document.querySelector(".js-value");
+    const formElement = document.querySelector(".js-form");
+    const exchangeResultElement = document.querySelector(".js-exchangeResult");
+    const exchangeElement = document.querySelector(".js-exchange");
+    
+    document.addEventListener("submit", (event) => {
+        event.preventDefault();
+    
+    
+        const value = valueElement.value;
+        const exchange = exchangeElement.value;
+    
+        let exchangeResult = value / exchange;
+    
+        exchangeResultElement.innerText = exchangeResult.toFixed(2);
+    });
+}
